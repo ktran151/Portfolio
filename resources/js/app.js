f661dc3651bbfdef78bfc3d4b8ed96bcdb82af16
ktran151 <ktran151@users.noreply.github.com>
2019-04-03 13:23:29 -0400

    projects updated with js removed

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,23 +4,5 @@ main()
 function main() {
 
 
-	//nav drop menu
-	addListener(dropbutton, 'click', event => {
-		toggleShowElement(nav)
-	})
-
-	//carousel
-	dotsToSlides();
-	showSlides(slideIndex);
-	// changeSlides('#prev', -1);
-	// changeSlides('#next', 1);
-	// const aboutPopup = document.querySelector('')
-
-	//project description
-	slideDescriptionListeners(slides, descriptions);
-
-	//modal
-	modalOn();
-
 }
 
